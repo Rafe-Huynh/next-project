f6f1d0cef22c840427e73dadda04bf15ca29fe83
Thai Huynh <rafehuynh0510@gmail.com>
2024-04-22 16:15:03 -0500

    handle edit and handle delete

---
+++ app/api/prompt/[id]/route.js
@@ -0,0 +1,39 @@
+import { connectToDB } from "../../../../utils/database"
+import Prompt from "../../../../models/prompt"
+// get 
+export const GET = async(request, {params}) => {
+    try {
+        await connectToDB()
+        const prompts = await Prompt.findById(params.id).populate('creator');
+        if(!prompts) return new Response("Prompt not found", {status:404})
+        return new Response(JSON.stringify(prompts), {status:200})
+    } catch (error) {
+        return new Response("failed to fetch", {status:500})
+    }
+}
+//patch
+export const PATCH = async(request, {params}) => {
+    const {prompt, tag} = await request.json()
+    try {
+        await connectToDB()
+        const existingPrompt = await Prompt.findById(params.id)
+        if(!existingPrompt) return new Response("Prompt not found", {status:404})
+        existingPrompt.prompt = prompt
+        existingPrompt.tag = tag
+        await existingPrompt.save()
+        return new Response(JSON.stringify(existingPrompt), {status:200})
+    } catch (error) {
+        return new Response("Failed to update the prompt", {status:500})
+    }
+}
+//delete
+export const DELETE = async(request, {params}) => {
+    
+    try {
+        await connectToDB
+        await Prompt.findByIdAndRemove(params.id)
+       
+    } catch (error) {
+        return new Response("Failed to delete prompt", {status:500})
+    }
+}
--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -5,7 +5,9 @@ import { useState, useEffect } from 'react'
 import { useSession } from 'next-auth/react'
 import { useRouter } from 'next/navigation'
 import Profile from '../../components/Profile/Profile'
+
 const MyProfile = () => {
+    const router = useRouter()
     const {data : session} = useSession()
     const [posts, setPosts] = useState([])
 
@@ -18,11 +20,22 @@ const MyProfile = () => {
     useEffect(() => {
         if(session?.user.id) fetchPosts()
     },[session?.user.id])
-    const handleEdit = () => {
-
+    const handleEdit = (post) => {
+      router.push(`/update-prompt?id=${post._id}`)
     }
-    const handleDelete = async () => {
-
+    const handleDelete = async (post) => {
+      const hasConfirmed = confirm("Are you sure you want to delete this prompt")
+      if(hasConfirmed){
+        try {
+          await fetch(`api/prompt/${post._id.toString()}`, {
+            method: "DELETE"
+          })
+          const filterPosts = posts.filter((p) => p._id !== post._id)
+          setPosts(filterPosts)
+        } catch (error) {
+          console.log(error)
+        }
+      }
     }
 
   return (
